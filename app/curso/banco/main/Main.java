7c59e158e02759c91f38e81ffe03202f104d98e4
KleyberJMH <kleyberjmh@gmail.com>
2022-11-04 16:46:43 +0100

    Verificacion de libreria

--- app/curso/banco/main/Main.java
+++ app/curso/banco/main/Main.java
@@ -1,9 +1,17 @@
 package app.curso.banco.main;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
+		try {
+			Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/banco", "banco", "banco");
+			System.out.println("Version del drive: " + conexion.getMetaData().getDriverVersion());
+		}catch(Exception e) {
+			System.out.println("Error");
+			e.printStackTrace();
+		}
 
 	}
 
